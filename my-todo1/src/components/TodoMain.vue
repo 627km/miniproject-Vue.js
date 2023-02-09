77f0e88c568f333c646248a6274fa17ca202cb42
627km <rudals_jeon@naver.com>
2023-02-09 14:50:53 +0900

    4. 삭제 기능

--- my-todo1/src/components/TodoMain.vue
+++ my-todo1/src/components/TodoMain.vue
@@ -3,13 +3,22 @@
         <header><h1>Vue Fire todo1</h1></header>
         <main>
             <div class="todos">
-                <div class="write">
-                <input 
-                ref="writeArea"
-                type="text" 
-                v-model="addItemText" 
-                @keyup.enter="addItem"/>
-                <button class="btn add" @click="addItem">Add</button>
+                <div class="write" v-if="writeState === 'add'"> <!-- 등록 -->
+                    <input 
+                        ref="writeArea"
+                        type="text" 
+                        v-model="addItemText" 
+                        @keyup.enter="addItem"/>
+                    <button class="btn add" @click="addItem">Add</button>
+                </div>
+
+                <div class="write" v-else> <!-- 수정 -->
+                    <input 
+                        ref="writeArea"
+                        type="text" 
+                        v-model="editItemText" 
+                        @keyup.enter="editSave"/>
+                    <button class="btn add" @click="editSave">Save</button>
                 </div>
                 <ul class="list">
                     <li v-for="(list, i) in todos" :key="i">
@@ -19,8 +28,8 @@
                         <span>
                         {{ list.text }}
                         <b>
-                            <a href="">Edit</a>
-                            <a href="">Del</a>
+                            <a href="" @click.prevent="editShow(i)">Edit</a>
+                            <a href="" @click.prevent="del(i)">Del</a>
                         </b>
                         </span>
                     </li>
@@ -34,7 +43,10 @@
     export default {
         data(){
             return{
+                writeState: 'add',
                 addItemText: '',
+                editItemText: '',
+                crrEditItem: '',
                 todos:[
                     {text: '공부하기', state: 'yet'},
                     {text: '운동하기', state: 'done'},
@@ -54,9 +66,22 @@
             checkItem(i) {
                 if (this.todos[i].state === 'yet') {
                     this.todos[i].state = 'done'
-                }else if (this.todos[i].state === 'done'){
+                }else {
                     this.todos[i].state = 'yet'
                 }
+            },
+            editShow(index){
+                this.crrEditItem = index;
+                this.writeState = 'edit';
+                this.editItemText = this.todos[index].text
+            },
+            editSave(){
+                this.todos[this.crrEditItem].text = this.editItemText;
+                this.editItemText = '';
+                this.writeState = 'add';
+            },
+            del(index) {
+                this.todos.splice(index, 1)
             }
         },
         mounted(){
