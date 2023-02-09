2974e0f3508faee2bb9500b7aef2f38137711681
627km <rudals_jeon@naver.com>
2023-02-09 14:14:04 +0900

    3. 체크 기능

--- my-todo1/src/components/TodoMain.vue
+++ my-todo1/src/components/TodoMain.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="page">
-      <header><h1>Vue Fire todo1</h1></header>
-          <main>
-          <div class="todos">
+        <header><h1>Vue Fire todo1</h1></header>
+        <main>
+            <div class="todos">
                 <div class="write">
-                <input type="text" v-model="addItemText"/>
+                <input 
+                ref="writeArea"
+                type="text" 
+                v-model="addItemText" 
+                @keyup.enter="addItem"/>
                 <button class="btn add" @click="addItem">Add</button>
                 </div>
                 <ul class="list">
-                    <li v-for="(list, i) in todos" :key="list.text">
-                        <i :class="[list.state === 'yet' ? 'far' : 'fas', 'fa-check-square']"></i>
+                    <li v-for="(list, i) in todos" :key="i">
+                        <i
+                        @click="checkItem(i)"
+                        :class="[list.state === 'yet' ? 'far' : 'fas', 'fa-check-square']"></i>
                         <span>
                         {{ list.text }}
                         <b>
@@ -19,36 +25,47 @@
                         </span>
                     </li>
                 </ul>
-          </div>
-          </main>
-      </div>
-  </template>
+            </div>
+        </main>
+    </div>
+</template>
   
-  <script>
-  export default {
-    data(){
-          return{
-            addItemText: '',
-            todos:[
-                {text: '공부하기', state: 'yet'},
-                {text: '운동하기', state: 'done'},
-                {text: '글쓰기', state: 'done'},
-            ]
-          }
-      },
-    methods: {
-        addItem(){
-            console.log(this.addItemText)
-            this.todos.push({
-                text: this.addItemText,
-                state: 'yet'
-            })
+<script>
+    export default {
+        data(){
+            return{
+                addItemText: '',
+                todos:[
+                    {text: '공부하기', state: 'yet'},
+                    {text: '운동하기', state: 'done'},
+                    {text: '글쓰기', state: 'done'},
+                ]
+            }
+        },
+        methods: {
+            addItem(){
+                if (this.addItemText === "") return;
+                this.todos.push({
+                    text: this.addItemText,
+                    state: 'yet'
+                })
+                this.addItemText = '';
+            },
+            checkItem(i) {
+                if (this.todos[i].state === 'yet') {
+                    this.todos[i].state = 'done'
+                }else if (this.todos[i].state === 'done'){
+                    this.todos[i].state = 'yet'
+                }
+            }
+        },
+        mounted(){
+            this.$refs.writeArea.focus()
         }
     }
-    }
   
-  </script>
+</script>
   
-  <style>
+<style>
   
-  </style>
+</style>
